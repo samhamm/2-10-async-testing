49958432fedd01754a93f7f6aba3dfb78c7a1ca8
samhamm <samhamm@gmail.com>
2015-02-11 00:42:49 -0800

    url parsing for greet/'name' works in browser, but not yet pasing tests

--- server.js
+++ server.js
@@ -3,26 +3,31 @@
 var http = require('http');
 var url = require('url');
 
+var time = new Date();
+var theTime = 'What time is it?!?! The time is ' +
+  ('0' + time.getHours()).slice(-2)   + ':' +
+  ('0' + time.getMinutes()).slice(-2) + ':' +
+  ('0' + time.getSeconds()).slice(-2) + ' muthafucka!';
+
 var server  = http.createServer(function (req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/plain',
   });
 
-  switch(req.url) {
-    case '/time':
-      var time = new Date();
-      var theTime = ('What time is it?!?! The time is ' +
-        ('0' + time.getHours()).slice(-2)   + ':' +
-        ('0' + time.getMinutes()).slice(-2) + ':' +
-        ('0' + time.getSeconds()).slice(-2) + ' muthafucka!');
+  var urlPath = url.parse(req.url).path.split('/');
+
+  if (urlPath[1] === 'greet')
+    var userName = urlPath[2];
+
+  switch(urlPath[1]) {
+    case 'time':
       res.write(theTime);
       break;
-    case '/greet':
-      var name = "fuckface";
-      res.write("Eat me, " + name + "!");
+    case 'greet':
+      res.write("Eat me, " + userName + "!");
       break;
     default:
-      res.write('No Route Here - 404');
+      res.write('No route here, sucker.');
     }
   res.end();
 });
